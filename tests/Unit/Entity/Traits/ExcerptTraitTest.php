a119c69bbd05616033b6d75f14231185d656dc73
manuxi <manu@bytefresser.de>
2021-04-03 02:11:52 +0200

    ExcerptTraitTest

--- tests/Unit/Entity/Traits/ExcerptTraitTest.php
+++ tests/Unit/Entity/Traits/ExcerptTraitTest.php
@@ -1,10 +1,30 @@
 <?php
 
-
 namespace Manuxi\SuluEventBundle\Tests\Unit\Entity\Traits;
 
+use Manuxi\SuluEventBundle\Entity\Traits\ExcerptTrait;
+use Sulu\Bundle\TestBundle\Testing\SuluTestCase;
 
-class ExcerptTraitTest
+class ExcerptTraitTest extends SuluTestCase
 {
+    private $mock;
 
+    protected function setUp(): void
+    {
+        $this->mock = $this->getMockForTrait(ExcerptTrait::class);
+
+    }
+
+    public function testSetId(): void
+    {
+        $this->assertSame($this->mock, $this->mock->setid(null));
+    }
+
+    public function testGetId(): void
+    {
+        $id = 42;
+        $this->assertNull($this->mock->getId());
+        $this->mock->setId($id);
+        $this->assertSame($id, $this->mock->getId());
+    }
 }
