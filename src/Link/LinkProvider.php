ec9a9dc1304ad39c6dee7d491ccd65373d88cc75
Manuel Bertrams <mail@bertrams-media.de>
2023-10-11 15:03:40 +0200

    Fixed locale in LinkProvider

--- src/Link/LinkProvider.php
+++ src/Link/LinkProvider.php
@@ -45,6 +45,7 @@ class LinkProvider implements LinkProviderInterface
         $result = [];
         $elements = $this->eventRepository->findBy(['id' => $hrefs]); // load items by id
         foreach ($elements as $element) {
+            $element->setLocale($locale);
             $result[] = new LinkItem($element->getId(), $element->getTitle(), $element->getRoutePath(), $element->isPublished()); // create link-item foreach item
         }
 
