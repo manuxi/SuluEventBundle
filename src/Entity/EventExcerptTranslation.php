82167b371e9d811c1a039cc1e2f540e08b256dc1
Manuel Bertrams <manuel@bertrams-media.de>
2025-10-19 23:26:58 +0200

    Category Fix

--- src/Entity/EventExcerptTranslation.php
+++ src/Entity/EventExcerptTranslation.php
@@ -6,29 +6,44 @@ namespace Manuxi\SuluEventBundle\Entity;
 
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\InverseJoinColumn;
 use Doctrine\ORM\Mapping\JoinColumn;
 use Doctrine\ORM\Mapping\JoinTable;
+use Doctrine\ORM\Mapping\ManyToMany;
 use Manuxi\SuluEventBundle\Repository\EventExcerptTranslationRepository;
 use Manuxi\SuluSharedToolsBundle\Entity\Abstracts\Entity\AbstractExcerptTranslation;
 use Manuxi\SuluSharedToolsBundle\Entity\Interfaces\ExcerptTranslationInterface;
+use Sulu\Bundle\CategoryBundle\Entity\Category;
+use Sulu\Bundle\MediaBundle\Entity\MediaInterface;
+use Sulu\Bundle\TagBundle\Tag\TagInterface;
 
 #[ORM\Entity(repositoryClass: EventExcerptTranslationRepository::class)]
 #[ORM\Table(name: 'app_event_excerpt_translation')]
 class EventExcerptTranslation extends AbstractExcerptTranslation implements ExcerptTranslationInterface
 {
-
+    #[ManyToMany(targetEntity: Category::class)]
     #[JoinTable(name: 'app_event_excerpt_categories')]
+    #[JoinColumn(name: 'excerpt_id', referencedColumnName: 'id')]
+    #[InverseJoinColumn(name: 'category_id', referencedColumnName: 'id')]
     protected ?Collection $categories = null;
 
+    #[ManyToMany(targetEntity: TagInterface::class)]
     #[JoinTable(name: 'app_event_excerpt_tags')]
+    #[JoinColumn(name: 'excerpt_id', referencedColumnName: 'id')]
+    #[InverseJoinColumn(name: 'tag_id', referencedColumnName: 'id')]
     protected ?Collection $tags = null;
 
+    #[ManyToMany(targetEntity: MediaInterface::class)]
     #[JoinTable(name: 'app_event_excerpt_icons')]
+    #[JoinColumn(name: 'excerpt_id', referencedColumnName: 'id')]
+    #[InverseJoinColumn(name: 'icon_id', referencedColumnName: 'id')]
     protected ?Collection $icons = null;
 
+    #[ManyToMany(targetEntity: MediaInterface::class)]
     #[JoinTable(name: 'app_event_excerpt_images')]
+    #[JoinColumn(name: 'excerpt_id', referencedColumnName: 'id')]
+    #[InverseJoinColumn(name: 'image_id', referencedColumnName: 'id')]
     protected ?Collection $images = null;
-
 
     public function __construct(
         #[ORM\ManyToOne(targetEntity: EventExcerpt::class, inversedBy: 'translations')]
