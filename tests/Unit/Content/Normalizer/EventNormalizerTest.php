08d93fe10cebf8d6b7ed72c8ae6c06aebdc21874
manuxi <11303615+manuxi@users.noreply.github.com>
2025-12-18 10:36:36 +0100

    Refactor EventNormalizerTest to include translator

--- tests/Unit/Content/Normalizer/EventNormalizerTest.php
+++ tests/Unit/Content/Normalizer/EventNormalizerTest.php
@@ -8,44 +8,48 @@ use Manuxi\SuluEventBundle\Content\Normalizer\EventNormalizer;
 use Manuxi\SuluEventBundle\Entity\Event;
 use Manuxi\SuluEventBundle\Entity\EventDimensionContent;
 use Manuxi\SuluEventBundle\Entity\Location;
+use Manuxi\SuluEventBundle\Service\EventTypeSelect;
 use PHPUnit\Framework\TestCase;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class EventNormalizerTest extends TestCase
 {
+    private $translator;
+    private $eventTypeSelect;
+    
+    protected function setUp(): void
+    {
+        $this->translator = $this->createMock(TranslatorInterface::class);
+        $this->translator->method('trans')->willReturn('Y-m-d');
+        $this->eventTypeSelect = $this->createMock(EventTypeSelect::class);
+        $this->eventTypeSelect->method('getTypeName')->willReturn('Test Type');
+    }
     public function testEnhanceLocationId(): void
     {
-        $normalizer = new EventNormalizer();
+        $normalizer = new EventNormalizer($this->translator, $this->eventTypeSelect);
         $event = $this->createMock(Event::class);
         $event->method('getId')->willReturn(123);
-
         $location = $this->createMock(Location::class);
         $location->method('getId')->willReturn(456);
-
         $dimensionContent = $this->createMock(EventDimensionContent::class);
         $dimensionContent->method('getResource')->willReturn($event);
         $dimensionContent->method('getLocation')->willReturn($location);
-
         $normalizedData = [];
         $result = $normalizer->enhance($dimensionContent, $normalizedData);
-
         $this->assertEquals(123, $result['id']);
         $this->assertEquals(456, $result['locationId']);
         $this->assertEquals(456, $result['location']['id']);
     }
-
     public function testEnhanceNoLocation(): void
     {
-        $normalizer = new EventNormalizer();
+        $normalizer = new EventNormalizer($this->translator, $this->eventTypeSelect);
         $event = $this->createMock(Event::class);
         $event->method('getId')->willReturn(123);
-
         $dimensionContent = $this->createMock(EventDimensionContent::class);
         $dimensionContent->method('getResource')->willReturn($event);
         $dimensionContent->method('getLocation')->willReturn(null);
-
         $normalizedData = [];
         $result = $normalizer->enhance($dimensionContent, $normalizedData);
-
         $this->assertEquals(123, $result['id']);
         $this->assertNull($result['locationId']);
         $this->assertNull($result['location']);
